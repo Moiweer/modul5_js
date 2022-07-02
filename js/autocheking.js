22e3e3384870751cc1b89a024c65a90087b548b9
Mariya Barska <moiweer@gmail.com>
2022-07-02 21:51:01 +0300

    11-15

--- js/autocheking.js
+++ js/autocheking.js
@@ -184,38 +184,218 @@
 
 // 10
 
-
-class Storage {
-    constructor(items){
-        this.items = items;
-    };
-    getItems() {
-        return this.items = this.items;
-    }
-        addItem(newItem) {
+// class Storage {
+//     constructor(items){
+//         this.items = items;
+//     };
+//     getItems() {
+//         return this.items;
+//     }
+//         addItem(newItem) {
        
-return this.items.push(newItem)
+// return this.items.push(newItem)
     
-    }
-removeItem(item) {
-    this.items.splice(this.items.indexOf(item), 1);
+//     }
+// removeItem(item) {
+//     this.items.splice(this.items.indexOf(item), 1);
+//   }
+
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// 11
+
+// class StringBuilder {
+//     constructor(value){
+//       this.value = value;
+//     }
+//     getValue(){
+//       return  this.value;
+//     }
+//     padEnd(str){
+//         this.value = this.value + str;
+//     }
+// padStart(str){
+//     this.value = str + this.value;
+// }
+// padBoth(str){
+//     this.value = str + this.value + str;
+// }
+// }
+
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// 12
+// class Car {
+//   #brand;
+//   model;
+//   price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand(){
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand){
+//     this.#brand = newBrand;
+//   }
+
+  
+// }
+
+// 13
+// class Storage {
+//   #items;
+
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+// 14
+
+//  class StringBuilder {
+// #value;
+
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// 15
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   #model;
+//   #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//  set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//  get model() {
+//     return this.#model;
+//   }
+
+//    set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+ 
+// }
+
+// 16
+
+class Car {
+ static  MAX_PRICE =  50000;
+
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
   }
 
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice <= this.MAX_PRICE){
+    this.#price = newPrice;}
+  }
+  
 }
 
-// Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
 
+audi.price = 49000;
+console.log(audi.price); // 49000
 
-
-
-
-
+audi.price = 51000;
+console.log(audi.price); // 49000
 
 // Перебираючі методи масиву
 
