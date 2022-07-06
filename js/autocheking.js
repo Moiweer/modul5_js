59abd7b372a176e94f68fff56ffe17fabc186c5c
Mariya Barska <moiweer@gmail.com>
2022-07-06 23:25:33 +0300

    18-19

--- js/autocheking.js
+++ js/autocheking.js
@@ -423,6 +423,74 @@
 // console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
 // 18
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser"}
+//   }
+
+// 19
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   constructor({email, accessLevel}){
+//     super(email);
+//     this.accessLevel = accessLevel
+   
+//   }
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+
+// 20
+
+
+
+
+
+
+
 
 
 // console.log(document);
